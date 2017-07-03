96320de76e300f60d8cb09313bae3d1971497d99
Michael Bilstein <micha3lbilstein@gmail.com>
2017-07-03 18:43:58 -0500

    add timestamp to hits

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ function createNewSite(site) {
 }
 
 function addNewHit(site, code) {
-	query(`INSERT INTO hits (site, code) VALUES (($1), ($2))`, [`${site}`, code])
+	query(`INSERT INTO hits (site, code, time) VALUES (($1), ($2), to_timestamp(($3))`, [`${site}`, code, new Date().getTime()])
 		.catch((error) => {
 			console.log(error);
 		});
